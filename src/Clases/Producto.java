f3d5c5f6672676bb137042ca5ed7c48d179074d8
jesus.raboso <jesus.rc93@gmail.com>
2022-02-11 17:39:17 +0100

    Creada la clase Producto con métodos sin implementar y Javadoc

---
+++ src/Clases/Producto.java
@@ -0,0 +1,66 @@
+package Clases;
+/**
+ * El objetivo de esta clase es la <b>multiplicación y potencia de números enteros y reales</b>.
+ * 
+ * Tendrá diferentes métodos para realizar acciones tales como multiplicar 2 número reales, 2 enteros,
+ * 3 reales o la potencia de un número.
+ * 
+ * @author Jesus
+ * @version 1.0
+ */
+public class Producto {
+	/**
+	 * Con este método introducimos dos números reales y nos
+	 * devuelve el producto de ambos.
+	 * 
+	 * @param numero1 Primer número real del producto.
+	 * @param numero2 Segundo número real del producto.
+	 * @return Multiplicación de ambos números.
+	 */
+	public static float multiplicacion(float numero1, float numero2) {
+		return 0;//Pongo 0 para que no de error
+	}
+	
+	/**
+	 * Con este método introducimos dos números enteros y nos
+	 * devuelve el producto de ambos.
+	 * 
+	 * @param numero1 Primer número entero del producto.
+	 * @param numero2 Segundo número real del producto.
+	 * @return Multiplicación de ambos números.
+	 */
+	public static int multiplicacion(int numero1, int numero2) {
+		return 0;//Pongo 0 para que no de error
+	}
+	
+	/**
+	 * Con este método introducimos tres números reales y nos
+	 * devuelve el producto de los tres.
+	 * 
+	 * @param numero1 Primer número real del producto.
+	 * @param numero2 Segundo número real del producto.
+	 * @param numero3 Tercer número real del producto.
+	 * @return Multiplicación de los tres números.
+	 */
+	public static float multiplicacion(float numero1, float numero2, float numero3) {
+		return 0;//Pongo 0 para que no de error
+	}
+	
+	/**
+	 * Con este método introducimos un número que será la base de la potencia
+	 * y otro número que será el exponente.
+	 * 
+	 * @param base La base de la potencia.
+	 * @param exponente El exponente de la potencia.
+	 * @return Resultado de la potencia.
+	 * @throws NumberIsTooLargeException El resultado desborda el tamaño máximo de un entero.
+	 */
+	public static int potencia(int base, int exponente) {
+		/*Tendríamos que controlar la excepción de si el 
+		 resultado es demasiado grande ya que, si no,
+		 el resultado sería incorrecto*/
+		
+		return 0;//Pongo 0 para que no de error
+	}
+
+}
